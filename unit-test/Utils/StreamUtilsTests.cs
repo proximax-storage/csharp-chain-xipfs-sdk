88ca4f4df4f9eb1b0c72298b7b88a52e544b8bb9
Tyrone <avanaur@gmail.com>
2018-11-27 13:28:16 +1100

    Added test for save to file

---
+++ unit-test/Utils/StreamUtilsTests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using IO.Proximax.SDK.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static UnitTests.TestSupport.Constants;
+
+namespace UnitTests.Utils
+{
+    [TestClass]
+    public class StreamUtilsTests
+    {
+        [TestMethod, Timeout(10000)] 
+        public void ShouldSaveToFile()
+        {
+            var tempFileName = Path.GetTempPath() + CurrentTimeMillis() + "-test.txt";
+            
+            new FileStream(TestTextFile, FileMode.Open, FileAccess.Read).SaveToFile(tempFileName);
+
+            var expected = new FileStream(TestTextFile, FileMode.Open, FileAccess.Read).GetContentAsString();
+            var actual = new FileStream(tempFileName, FileMode.Open, FileAccess.Read).GetContentAsString();
+            
+            Assert.AreEqual(expected, actual);
+        }
+        
+        private static long CurrentTimeMillis()
+        {
+            return (long) (DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds;
+        }
+        
+   }
+}
