134a71a635b9267892b68c5d3b68ade709a2cf70
Tyrone <avanaur@gmail.com>
2018-11-29 15:38:10 +1100

    Added search implementation

---
+++ xpx2-csharp-sdk/Exceptions/SearchFailureException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IO.Proximax.SDK.Exceptions
+{
+    public class SearchFailureException : SystemException
+    {
+        public SearchFailureException(string message) : base(message) { }
+
+        public SearchFailureException(string message, Exception innerException): base(message, innerException) { }
+    }
+}
